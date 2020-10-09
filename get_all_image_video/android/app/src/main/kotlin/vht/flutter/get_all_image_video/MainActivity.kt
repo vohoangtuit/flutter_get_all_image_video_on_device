7516e31e6a8727ad3f86316c87604441c2ab8fbd
vohoangtuit <vohoangtu2401@gmail.com>
2020-10-09 16:23:25 +0700

    create project

---
+++ get_all_image_video/android/app/src/main/kotlin/vht/flutter/get_all_image_video/MainActivity.kt
@@ -0,0 +1,6 @@
+package vht.flutter.get_all_image_video
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ get_all_image_video/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Get Image Video',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+       centerTitle: true,
+        title: Text("Image Video"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            GestureDetector(
+              child: Text(
+                'Welcome to flutter', style: TextStyle(fontSize: 20, color: Colors.red),
+              ),
+              onTap: (){
+                print("on click");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
